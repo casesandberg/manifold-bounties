e0bcbc715bd2ffeedc47dd63e4ef9f62773961ad
case <case@casesandberg.com>
2023-12-01 11:58:54 -0800

    Init Layout

---
+++ app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/Container";
+import { Hero } from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Container>foo</Container>
+    </>
+  );
+}
